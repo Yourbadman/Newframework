0043cd709aceaadd602a9e056a44304e72cd0016
jsonwong <wrd666520@163.com>
2016-04-05 20:12:50 +0800

    'addPhotoView'

---
+++ app/src/main/java/com/jsonwong/newframework/ui/demo/PhotoApp.java
@@ -0,0 +1,12 @@
+package com.jsonwong.newframework.ui.demo;
+
+import android.app.Application;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class PhotoApp extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(getApplicationContext());
+    }
+}
